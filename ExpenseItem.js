7ac09cf9cb86ba4ff75f7cb8a79405b53cb0595f
mudassirpathan1 <113206188+mudassirpathan1@users.noreply.github.com>
2023-01-05 20:23:24 +0530

    ok

---
+++ ExpenseItem.js
@@ -0,0 +1,6 @@
+
+function ExpenseItem(){
+    return <h2> Expense Item here!</h2>
+}
+
+export default ExpenseItem;
---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ExpenseItem(props) {
+  return (
+    <div>
+      <h1>Expense Items</h1>
+      <ul>
+        <li>Food Rs 10</li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 200</li>
+      </ul>
+    </div>
+  );
+}
